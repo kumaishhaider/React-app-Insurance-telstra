c547fe05d351bef5cc427e5ddf3b2ece212432dd
kumaishhaider <135639418+kumaishhaider@users.noreply.github.com>
2025-05-21 04:50:01 -0700

    Deploy site with CNAME and final setup

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Telstra Secure Insurance</h1>
+      <p>Your insurance portal is live!</p>
+    </div>
+  );
+}
+
+export default App;
